f79eadacb4ab24a81e41874b4fe167582e6396ad
Thomas <thomasnw@uia.no>
2023-03-14 14:23:23 +0100

    GDT added

--- group_1/src/kernel/src/main.cpp
+++ group_1/src/kernel/src/main.cpp
@@ -1,9 +1,8 @@
-#include "gdt.h"
-#include "common.h"
-
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
     #include "system.h"
+    #include "gdt.h"
+    #include "common.h"
     void kernel_main();
 }
 
@@ -12,6 +11,10 @@ extern "C" void test_gdt();
 void kernel_main()
 {
    init_descriptor_tables();
-   //write_string(15, "Hello world!");
-   test_gdt();
+   
+   if (is_gdt_implemented)
+   {
+        write_string(15, "Hello world!");
+        }
+   //test_gdt();
 }
